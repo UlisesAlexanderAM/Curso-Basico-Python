4b0d280dea66db8991c2de0a873bfd48346d2297
Ulises Alexander Arguelles Monjaraz <ulises_alexander_am@protonmail.com>
2021-10-14 13:52:56 -0500

    Uso de condicionales y función

--- conversion.py
+++ conversion.py
@@ -1,18 +1,23 @@
-#!/usr/bin/env python3
+def conversor(razon_de_cambio, monto):
+    monto = monto / razon_de_cambio
+    monto = str(round(monto, 2))
+    return monto
 
-pesos_mexicanos = input("¿Cuantos pesos mexicanos(MXN) tienes?: ")
-pesos_mexicanos = float(pesos_mexicanos)
-valor_dolar = 20.75
-dolares = pesos_mexicanos / valor_dolar
-dolares = round(dolares, 2)
-dolares = str(dolares)
 
-usd = input("¿Cuantos dolares estadounidenses(USD) tienes?: ")
-usd = float(usd)
-valor_mxn = 0.04
-mxn = usd / valor_mxn
-mxn = round(mxn, 2)
-mxn = str(mxn)
-
-print("Los pesos mexicanos que tienes equivalen a $" + dolares + " USD")
-print("Los dolares estadounidenses que tienes equivalen a $" + mxn + " MXN")
+menu = """
+Bienvenido, este es un conversor de monedas.
+Elige una de las siguiente opciones:
+1- Pesos Mexicano a Dolares Estadounidenses (MXN/USD)
+2- Dolares Estadounidenses a Pesos Mexicanos (USD/MXN)
+ """
+opcion = input(menu)
+if opcion == "1":
+    pesos_mexicanos = input("¿Cuántos pesos mexicanos(MXN) tienes?: ")
+    pesos_mexicanos = float(pesos_mexicanos)
+    conversor(20.75, pesos_mexicanos)
+    print("Los pesos mexicanos que tienes equivalen a $" + dolares + " USD")
+elif opcion == "2":
+    usd = input("¿Cuántos dólares estadounidenses(USD) tienes?: ")
+    usd = float(usd)
+    conversor(0.4, usd)
+    print("Los dólares estadounidenses que tienes equivalen a $" + mxn + " MXN")
